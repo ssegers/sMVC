7a27fa84f5fc5783afec039d9e26790664116e02
u0067341 <u0067341@LT3210046.int.ucll.be>
2023-08-02 16:38:35 +0200

    code enhancement

--- core/Database.php
+++ core/Database.php
@@ -1,5 +1,10 @@
 <?php
 
+namespace core;
+
+use PDO;
+use PDOException;
+
 /**
  * sMVC database class
  *
@@ -8,22 +13,16 @@
  * @version    1.0.0
  * @since      Class available since Release 1.0.0
  */
-
-namespace core;
-
-use PDO;
-
-/**
- * Description of Database
- *
- * @author seger
- */
 class Database
 {
     public \PDO $pdo;
     protected string $migrationPath;
 
-    public function __construct($dbConfig = [])
+    /**
+     * @param array<mixed> $dbConfig
+     * @return void
+     */
+    public function __construct(array $dbConfig = []) : void
     {
         $this->migrationPath = (Application::$ROOT_DIR . '\database\migrations\\');
         $dbDsn = 'mysql:host=' . $dbConfig['host'] . ';port=3306;dbname=' . $dbConfig['name'];
@@ -37,37 +36,52 @@ class Database
         }
     }
 
-    public function migrate()
+    /**
+     * executes the new migrations
+     * 
+     * @return void
+     */
+    public function migrate(): void
     {
         $this->createMigrationsTable();
         $migrated = $this->getExecutedMigrations();
 
         $newMigrations = [];
-        $allMigrations = array_slice(scandir($this->migrationPath), 2);
-        $toMigrate = array_diff($allMigrations, $migrated);
-        if (!empty($toMigrate)) {
-            foreach ($toMigrate as $migration) {
-                require_once $this->migrationPath . $migration;
-                $filename = pathinfo($migration, PATHINFO_FILENAME);
-                $classname = $this->getClassname($filename);
-                $instance = new $classname();
-                $this->showMessage("migrating $migration");
-                $sql = $instance->up();
-                try {
-                    $this->pdo->exec($sql);
-                    $this->showMessage("migrated $migration");
+        $directoryTree = scandir($this->migrationPath);
+        if ($directoryTree) {
+            $allMigrations = array_slice($directoryTree, 2);//removes . and .. from the directory tree
+            $toMigrate = array_diff($allMigrations, $migrated);
+            if (!empty($toMigrate)) {
+                foreach ($toMigrate as $migration) {
+                    require_once $this->migrationPath . $migration;
+                    $filename = pathinfo($migration, PATHINFO_FILENAME);
+                    $classname = $this->getClassname($filename);
+                    $instance = new $classname();
+                    $this->showMessage("migrating $migration");
+                    $sql = $instance->up();
+                    try {
+                        $this->pdo->exec($sql);
+                        $this->showMessage("migrated $migration");
 
-                    $this->saveMigrations($migration);
-                } catch (PDOException $error) {
-                    $this->showMessage("There is an error in your migration code: " . $error->getMessage());
+                        $this->saveMigration($migration);
+                    } catch (PDOException $error) {
+                        $this->showMessage("There is an error in your migration code: " . $error->getMessage());
+                    }
                 }
+            } else {
+                $this->showMessage("There are no migrations to execute");
             }
         } else {
             $this->showMessage("There are no migrations to execute");
         }
     }
-
-    protected function createMigrationsTable()
+    
+    /**
+     * creates the migrations table in the database if not exists 
+     * 
+     * @return void
+     */
+    protected function createMigrationsTable(): void
     {
         $this->pdo->exec("CREATE TABLE IF NOT EXISTS migrations (
             id INT AUTO_INCREMENT PRIMARY KEY,
@@ -75,16 +89,27 @@ class Database
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )  ENGINE=INNODB;");
     }
-
-    protected function getExecutedMigrations()
+    
+    /**
+     * gets all the executed migrations from the migrations table in the database
+     * 
+     * @return array<string> names of executed migrations
+     */
+    protected function getExecutedMigrations(): array
     {
         $statement = $this->pdo->prepare("SELECT migration FROM migrations");
         $statement->execute();
-
-        return $statement->fetchAll(\PDO::FETCH_COLUMN);
+        $executedMigrations = $statement->fetchAll(\PDO::FETCH_COLUMN);
+        return $executedMigrations;
     }
 
-    protected function saveMigrations(string $newMigration)
+    /**
+     * saves the given migration into the database
+     * 
+     * @parm string $newMigration
+     * @return void
+     */
+    protected function saveMigration(string $newMigration): void
     {
         //create string to insert
         //"('migration_file_name1'),('migration_file_name2'), ..."
@@ -96,9 +121,15 @@ class Database
         $statement->execute();
     }
 
-    protected function getClassname(string $functionname)
+    /**
+     * gets the classname from the filename
+     * 
+     * @parm string $fileName
+     * @return string
+     */
+    protected function getClassname(string $fileName): string
     {
-        $parts = explode('_', $functionname);
+        $parts = explode('_', $fileName);
         $classname = "";
         for ($partnum = 4; $partnum < count($parts); $partnum++) {
             $classname .= ucfirst($parts[$partnum]);
@@ -106,52 +137,58 @@ class Database
         return $classname;
     }
 
-    protected function showMessage($message)
+    /**
+     * shows a message in the console
+     * 
+     * @parm string $message
+     * @return void
+     */
+    protected function showMessage(string $message): void
     {
         echo "[" . date("Y-m-d H:i:s") . "] - " . $message . PHP_EOL;
     }
 
    /**
-     * Create a new migration in the database/migrations folder.
+     * Create a new migration in the /database/migrations folder.
      *
      * @param  string  $migrationName
-     * @return boolean
-     *
-     * @throws \InvalidArgumentException
+     * @return mixed
      */
-    public function createNewMigration($migrationName)
+    public function createNewMigration(string $migrationName) : mixed
     {
         $migrationClassName = $this->createMigrationClassName($migrationName);
-        if ($this->migrationAlreadyExist($migrationClassName)) {
+        if ($this->checkIfMigrationAlreadyExist($migrationClassName)) {
             return false;
+        } else {
+            $fileName = $this->getDatePrefix() . "_" . $migrationName . ".php";
+            $data = $this->migrationClassData($migrationClassName);
+            file_put_contents($this->migrationPath . $fileName, $data);
+            return $fileName;
         }
-        $fileName = $this->getDatePrefix() . "_" . $migrationName . ".php";
-        $data = $this->migrationClassData($migrationClassName);
-        file_put_contents($this->migrationPath . $fileName, $data);
-        return $fileName;
     }
     /**
      * Ensure that a migration with the given name doesn't already exist.
      *
-     * @param  string  $name
-     * @param  string  $migrationPath
-     * @return void
-     *
-     * @throws \InvalidArgumentException
+     * @param  string  $migrationName
+     * @return bool
      */
-    protected function migrationAlreadyExist($migrationName)
+    protected function checkIfMigrationAlreadyExist(string $migrationName): bool
     {
         if (! empty($this->migrationPath)) {
-            $migrationFiles = array_slice(scandir($this->migrationPath), 2);
-            foreach ($migrationFiles as $migrationFile) {
-                require_once($this->migrationPath . $migrationFile);
+            $directoryTree = scandir($this->migrationPath);
+            if ($directoryTree){
+                $migrationFiles = array_slice($directoryTree, 2);//removes . and .. from the directory tree
+                foreach ($migrationFiles as $migrationFile) {
+                    require_once($this->migrationPath . $migrationFile);
+                }
             }
         }
 
         if (class_exists($migrationName)) {
             return true;
+        }else {
+            return false;
         }
-        return false;
     }
 
     /**
@@ -159,7 +196,7 @@ class Database
      *
      * @return string
      */
-    protected function getDatePrefix()
+    protected function getDatePrefix(): string
     {
         return date('Y_m_d_His');
     }
@@ -167,9 +204,10 @@ class Database
     /**
      * creates UperCamelCase ClassName based on the migrations name.
      *
+     * @param string $migrationName
      * @return string
      */
-    protected function createMigrationClassName($migrationName)
+    protected function createMigrationClassName(string $migrationName): string
     {
         $migrationClassName = "";
         $migrationNameParts = explode("_", $migrationName);
@@ -179,7 +217,13 @@ class Database
         return $migrationClassName;
     }
 
-    protected function migrationClassData($classname)
+    /**
+     * return predefined content for the migration class.
+     *
+     * @param string $classname
+     * @return string
+     */
+    protected function migrationClassData(string $classname): string
     {
         $data = <<<EOF
         <?php
